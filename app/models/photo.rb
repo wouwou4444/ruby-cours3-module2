8bf8ee1f78425454c28a8e0a92965889452f3390
wouwou4444 <valery.buz@outlook.com>
2017-08-04 08:15:52 +0200

    photo 3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,4 +7,13 @@ class Photo
 	def self.mongo_client
 		@@db = Mongoid::Clients.default
 	end
+
+	def initialize (hash=nil)
+		if !hash.nil?
+			if !hash[:_id].nil?
+				@id=hash[:_id].to_s
+			end
+			@location = hash[:metadata].nil? ? nil : Point.new(hash[:metadata][:location])
+		end
+	end
 end
