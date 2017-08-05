3b236243efcfb1a345e916eabe3be77c2e9171f1
wouwou4444 <valery.buz@outlook.com>
2017-08-05 14:19:53 +0200

    photo 4

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
+require 'exifr/jpeg'
+
 class Photo
 
 	attr_accessor :id, :location
@@ -14,6 +16,25 @@ class Photo
 				@id=hash[:_id].to_s
 			end
 			@location = hash[:metadata].nil? ? nil : Point.new(hash[:metadata][:location])
+		end
+	end
+
+	def persisted?
+		return @id.nil? ? false : true
+	end
+
+	def save
+		if !self.persisted?
+			gps = EXIFR::JPEG.new(@contents).gps
+			@location = Point.new(:lng=>gps.longitude, :lat=>gps.latitude)
+			@contents.rewind
+			description = {}
+			description[:content_type] = "image/jepg"
+			description[:metadata]={:file=>@location.to_hash[:type],:location=>@location.to_hash[:coordinates]}
+			grid_file = Mongo::Grid::File.new(@contents.read, description)
+			id = Photo.mongo_client.database.fs.insert_one(grid_file)
+			@id = id.to_s
+
 		end
 	end
 end
